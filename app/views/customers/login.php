5759272d44aa166dc91f88067bd29b45f60eb71a
NatSison <111641301+NatSison@users.noreply.github.com>
2023-10-16 18:22:19 +0800

    Integrated Login Design to System

--- app/views/customers/login.php
+++ app/views/customers/login.php
@@ -1,28 +1,51 @@
-<?php require APPROOT . '/views/templates/header.php'; ?>
-	<div class="row justify-content-center" style="height: 100%">
-		<div class="card p-3 border col-4 align-middle">
-			<div class="card-title">
-				<h4 class="text-center">Login</h4>
-			</div>
-			<div class="card-body">
-				<form method="post" action="#">
-                    <label for="username">User Name: <sup>*</sup></label>
-                    <input type="text" name="username" class="form-control form-control-lg" value="" required>
-                    <span class="invalid-feedback"><?php echo (isset($data["error"]["username_err"]) ? $data["error"]["username_err"] : "") ?></span>
-                    
-                    <label for="password">Password: <sup>*</sup></label>
-                    <input type="password" name="password" class="form-control form-control-lg" value="" required>
-                    <span class="invalid-feedback"><?php echo (isset($data["error"]["password"]) ? $data["error"]["password_err"] : "") ?></span>
+<?php
+  //require APPROOT . '/views/templates/header.php';
+?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+    <link rel="stylesheet" href="<?php echo URLROOT . '/login.css' ?>" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="..."
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+    />
+  </head>
 
-                    <span class="invalid-feedback"><?php echo (isset($data["error"]["login_err"]) ? $data["error"]["login_err"] : "") ?></span>
-                    <br/>
-					<div class="d-flex justify-content-center">
-						<input class="btn btn-primary px-5" name="submit" type="submit" id="submit" value="Login">
-					</div><br/>
-					<p>New User? <a class="btn btn-success" href="<?php echo URLROOT."/customers/signup" ?>">Register Here</a></p>
-				</form>
-			</div>
-		</div>
-	</div>
+  <body>
+    <div class="login-container">
+      <div class="logo">
+        <img src="<?php echo URLROOT.'/img/Logo.png'?>" alt="" class="logo-image">
+        <h2>Login</h2>
+      </div>
+      <form method="post" action="#">
+        <div class="input-field">
+          <label for="username">User Name: <sup>*</sup></label>
+          <input type="text" name="username" class="form-control form-control-lg" value="" required>
+          <span class="invalid-feedback"><?php echo (isset($data["error"]["username_err"]) ? $data["error"]["username_err"] : "") ?></span>
+        </div>
+        <div class="input-field">
+          <label for="password">Password: <sup>*</sup></label>
+          <input type="password" name="password" class="form-control form-control-lg" value="" required>
+          <span class="invalid-feedback"><?php echo (isset($data["error"]["password"]) ? $data["error"]["password_err"] : "") ?></span>
+        </div>
+        <input class="btn btn-primary px-5" name="submit" type="submit" id="submit" value="Login">
+      </form>
+    </div>
 
-<?php require APPROOT . '/views/templates/footer.php'; ?>
+  
+    <script src="login.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.bundle.min.js"
+      integrity="..."
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
--- app/views/test/index.php
+++ app/views/test/index.php
@@ -1,12 +1,11 @@
 <?php
-
+  //require APPROOT . '/views/templates/header.php';
 ?>
-<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login Page</title>
+    <title><?php echo $data['title']?></title>
     <link rel="stylesheet" href="login.css" />
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css"
@@ -26,20 +25,23 @@
         <img src="<?php echo URLROOT.'/img/Logo.png'?>" alt="" class="logo-image">
         <h2>Login</h2>
       </div>
-      <form onsubmit="return login()">
+      <form method="post" action="#">
         <div class="input-field">
           <p style="text-align: left;">Username:</p>
-          <label for="loginUsername"></label>
-          <input type="text" id="loginUsername" name="loginUsername" required>
+          <label for="username">User Name: <sup>*</sup></label>
+          <input type="text" name="username" class="form-control form-control-lg" value="" required>
+          <span class="invalid-feedback"><?php echo (isset($data["error"]["username_err"]) ? $data["error"]["username_err"] : "") ?></span>
         </div>
         <div class="input-field">
           <p style="text-align: left;">Password:</p>
-          <label for="loginPassword"></label>
-          <input type="password" id="loginPassword" name="loginPassword" required>
+          <label for="password">Password: <sup>*</sup></label>
+          <input type="password" name="password" class="form-control form-control-lg" value="" required>
+          <span class="invalid-feedback"><?php echo (isset($data["error"]["password"]) ? $data["error"]["password_err"] : "") ?></span>
         </div>
-        <button type="submit">Login</button>
+        <input class="btn btn-primary px-5" name="submit" type="submit" id="submit" value="Login">
       </form>
     </div>
+
   
     <script src="login.js"></script>
     <script
